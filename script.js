dd7456ac7dee762a3e0e9c7388d2944a198ab65a
livujjwal <ujjwalsingh2112@gmail.com>
2023-08-12 09:39:43 +0530

    getting result on click of btn

--- script.js
+++ script.js
@@ -8,20 +8,55 @@ let arr = [
 // Please don't change anything in the index.html file.
 
 function PrintDeveloper() {
-  //Write your code here , just console.log
+  let ans = [];
+  // for loop to ietrate through array
+  for(let i of arr)
+  {
+    // check for profession in each obj
+    if(i["profession"] == "developer"){
+   ans.push(i);
+  }
+  }
+  console.log(ans);
 }
+// console.log(PrintDeveloper());
 
 function addData() {
-  //Write your code here, just console.log
+  // push obj using push mathod at index 3.
+ arr.push({id:4,name:"susan",age:"20",profession:"intern"})
+  // return arr;//
+  console.log(arr);
 }
+// console.log(addData());
 
 function removeAdmin() {
-  //Write your code here, just console.log
+     // for loop to ietrate through array
+  for(let i of arr)
+  {
+    // check for profession in each obj
+    if(i["profession"] == "admin"){
+      let index = i["id"]-1;
+      // use splice method to delete admin employee
+      arr.splice(index, 1);
+  }
+  }
+  console.log(arr);
+  // return arr;
 }
+// console.log(removeAdmin());
+
 
 function concatenateArray() {
-  //Write your code here, just console.log
+  let employee = [
+    { id: 5, name: 'jack', age: 18, profession: 'intern'},
+    { id: 6, name: 'jonny', age: 34, profession: 'finance'},
+    { id: 7, name: 'jasmin', age: 28, profession: 'hr'},
+    { id: 8, name: 'jenny', age: 38, profession: 'manager'}
+  ]
+   console.log(arr.concat(employee));
+  //  return arr.concat(employee);
 }
+// console.log(concatenateArray())
 
 // Here is an example of how functions work,
 // basically a function is a block of code which can directly access your 'arr' variable since arr is global.
